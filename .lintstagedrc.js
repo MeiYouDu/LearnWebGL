cbe7eb32644d58825261119d78f7d07f78d8b8a5
没有毒 <y836245751@gmail.com>
2025-08-30 23:08:06 +0800

    build: gitlab cicd config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,4 +3,8 @@ module.exports = {
 		"pnpm exec eslint --fix",
 	],
 	"*.{css,sass,scss}": ["pnpm exec stylelint --fix"],
+	// 整个工程
+	"**/*.{js,ts,jsx,tsx,vue,json,md}": [
+		"npx eslint -c eslint.config.mjs --fix ./",
+	],
 };
