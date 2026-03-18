53879c2776943a3f784c1716793850f4a29275f4
没有毒 <y836245751@gmail.com>
2026-03-18 15:07:55 +0800

    feat: camera optional near & far

--- src/helperv1/camera.ts
+++ src/helperv1/camera.ts
@@ -5,6 +5,8 @@ interface CameraConstructorOptions {
 	position?: vec3;
 	front?: vec3;
 	up?: vec3;
+	near?: number;
+	far?: number;
 }
 /**
  * 相机类
@@ -14,6 +16,8 @@ class Camera {
 		this.position = options?.position || vec3.fromValues(0, -3, 0);
 		this.front = options?.front || vec3.fromValues(0, 1, 0);
 		this.up = options?.up || vec3.fromValues(0, 0, 1);
+		this.near = options?.near ?? this.near;
+		this.far = options?.far ?? this.far;
 	}
 	public position: vec3;
 	public front: vec3;
@@ -21,6 +25,8 @@ class Camera {
 	public viewMatrix: mat4 = mat4.identity(mat4.create());
 	public quaternion: quat = quat.create();
 	public projectionMatrix: mat4 = mat4.identity(mat4.create());
+	public near: number = 1;
+	public far: number = 1000;
 
 	public render(gl: WebGL2RenderingContext) {
 		this.updateViewMatrix();
@@ -38,8 +44,8 @@ class Camera {
 			this.projectionMatrix,
 			pi / 4,
 			gl.canvas.width / gl.canvas.height,
-			1,
-			Number.POSITIVE_INFINITY,
+			this.near,
+			this.far,
 		);
 	}
 	private updateViewMatrix() {
