5bb77ca901ea9c0301ddf66ec133a26e2050faf9
没有毒 <y836245751@gmail.com>
2026-03-20 17:42:40 +0800

    feat: point texture attribute pointer

--- src/helperv1/utils/vertexAttribPointer.ts
+++ src/helperv1/utils/vertexAttribPointer.ts
@@ -26,5 +26,26 @@ function PNTAttribPointer(gl: WebGL2RenderingContext, material: Material): numbe
 	}
 	return stride;
 }
+/**
+ * position texture
+ * @param gl
+ * @param material
+ * @returns
+ */
+function PTAttribPointer(gl: WebGL2RenderingContext, material: Material): number {
+	const stride = 5;
+	const positionAttrLocation = material.getAttribLocation("position");
+	const texCoordAttrLocation = material.getAttribLocation("texCoord");
 
-export { PNTAttribPointer };
+	if (typeof positionAttrLocation === "number" && positionAttrLocation >= 0) {
+		gl.vertexAttribPointer(positionAttrLocation, 3, gl.FLOAT, false, stride * 4, 0);
+		gl.enableVertexAttribArray(positionAttrLocation);
+	}
+	if (typeof texCoordAttrLocation === "number" && texCoordAttrLocation >= 0) {
+		gl.vertexAttribPointer(texCoordAttrLocation, 2, gl.FLOAT, false, stride * 4, 3 * 4);
+		gl.enableVertexAttribArray(texCoordAttrLocation);
+	}
+	return stride;
+}
+
+export { PNTAttribPointer, PTAttribPointer };
