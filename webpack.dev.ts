f02459927652af032b090e7b4c115ec6be96a156
没有毒 <y836245751@gmail.com>
2026-08-05 00:29:57 +0800

    build: webpack eslint plugin config add shader file extension

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -1,8 +1,8 @@
 import { Configuration, DefinePlugin } from "webpack";
 import type { Configuration as ServerConfiguration } from "webpack-dev-server";
 import { merge } from "webpack-merge";
-import { EXCLUDECSS } from "./constant.ts";
-import common from "./webpack.common.ts";
+import { EXCLUDECSS } from "./constant";
+import common from "./webpack.common";
 
 interface Config extends Configuration {
 	devServer?: ServerConfiguration;
